03d28f9d584eb354c24372d72c858eff5767d180
Hoelito1404 <hoelito.lima@gmail.com>
2023-05-19 21:09:06 -0400

    commit firts

---
+++ src/Main_Quebra_de_linha.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main_Quebra_de_linha {
+
+	public static void main(String[] args) {
+		
+		 Scanner Sc = new Scanner(System.in);
+		 int x;
+		 
+		 String s1,s2,s3;
+		 
+		 x =Sc.nextInt();
+		 
+		 Sc.nextLine();
+		 s1 = Sc.nextLine();
+		 s2 = Sc.nextLine();
+		 s3 = Sc.nextLine();
+       
+		 System.out.println("DADOS DIGITADOS");
+		 System.out.println(x);
+		 System.out.println(s1);
+		 System.out.println(s2);
+		 System.out.println(s3);
+		 
+		 
+		 
+		 
+		 
+		Sc.close();
+			
+		 
+	}
+
+}
---
+++ src/Main_double.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main_double {
+
+	public static void main(String[] args) {
+	    
+		Scanner sc =new Scanner (System.in);
+		
+		double x;
+		x = sc.nextDouble();
+		System.out.printf("voce digitou:%.2f%n" , x);
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
